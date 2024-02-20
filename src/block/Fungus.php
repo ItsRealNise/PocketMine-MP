453fa671bd781c887a028e973fc5a77b7cbd6934
pandaaabe <135014092+pandaa-be@users.noreply.github.com>
2024-02-20 11:43:30 +0100

    add fungus class

---
+++ src/block/Fungus.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+ */
+
+declare(strict_types=1);
+
+namespace pocketmine\block;
+
+use pocketmine\block\utils\BlockEventHelper;
+use pocketmine\item\Fertilizer;
+use pocketmine\item\Item;
+use pocketmine\item\ItemTypeIds;
+use pocketmine\math\Facing;
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
+use pocketmine\utils\Random;
+use pocketmine\world\BlockTransaction;
+use pocketmine\world\ChunkManager;
+use pocketmine\world\generator\object\WarpedFungi;
+use pocketmine\world\Position;
+use function count;
+use function mt_rand;
+
+class Fungus extends Opaque{
+
+    //@todo add moss, breeding logic, warped fungus on stick, compositing
+
+    //This block allows placement of fungus.
+    private const PLACEMENT = [BlockTypeIds::GRASS, BlockTypeIds::DIRT, BlockTypeIds::PODZOL, BlockTypeIds::FARMLAND, BlockTypeIds::CRIMSON_NYLIUM, BlockTypeIds::WARPED_NYLIUM, BlockTypeIds::MYCELIUM, BlockTypeIds::SOUL_SOIL, BlockTypeIds::MUD, BlockTypeIds::MUDDY_MANGROVE_ROOTS, BlockTypeIds::FLOWER_POT];
+
+    public function ticksRandomly() : bool{
+        return true;
+    }
+
+    public function onNearbyBlockChange(): void
+    {
+        if($this->getSide(Facing::DOWN)->isTransparent()){
+            $this->position->getWorld()->useBreakOn($this->position);
+        }
+    }
+
+    public function place(BlockTransaction $tx, Item $item, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector, ?Player $player = null): bool
+    {
+        if(in_array($this->getSide(Facing::DOWN)->getTypeId(), self::PLACEMENT)){
+            return false;
+        }
+
+        return true;
+    }
+
+    public function onInteract(Item $item, int $face, Vector3 $clickVector, ?Player $player = null, array &$returnedItems = []) : bool{
+        if($this->getSide(Facing::UP)->getTypeId() !== BlockTypeIds::AIR){
+            return false;
+        }
+
+        if($item instanceof Fertilizer && (new WarpedFungi())?->getBlockTransaction($this->position->world, $this->position->x, $this->position->y, $this->position->z, new Random(mt_rand()))){
+            $item->pop();
+
+            return true;
+        }
+
+        return false;
+    }
+}
